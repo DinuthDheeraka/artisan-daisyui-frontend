90d13e94af5a8e038ad9c9af4135f0b2059b7e5f
dinuthd <dinuthd@ceyentra.com>
2024-05-04 11:02:05 +0530

    added PaginationButtonGroup.jsx

---
+++ src/components/pagination-button-group/PaginationButtonGroup.jsx
@@ -0,0 +1,9 @@
+export default function PaginationButtonGroup() {
+    return (
+        <div className="join">
+            <button className="join-item btn">«</button>
+            <button className="join-item btn">Page 22</button>
+            <button className="join-item btn">»</button>
+        </div>
+    );
+}
